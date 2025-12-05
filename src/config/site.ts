0500db4f9045f4946bff7369880e6e0377ae0af9
0xMichaelRan <michaelran16@gmail.com>
2025-12-05 22:12:11 +0800

    hjelllo test

--- src/config/site.ts
+++ src/config/site.ts
@@ -86,6 +86,15 @@ export const products: Product[] = [
                     },
                 ],
             },
+            heloworld: {
+                environments: [
+                    {
+                        name: "Hello dashboard",
+                        url: "https://example.google.xyz/",
+                        description: "database viewing dashbaord",
+                    },
+                ],
+            },
             other: {
                 environments: [
                     {
