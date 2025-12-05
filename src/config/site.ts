365bef9de980619e3192de964d1858ef5f9e4809
0xMichaelRan <michaelran16@gmail.com>
2025-12-05 22:06:49 +0800

    add other env and test content

--- src/config/site.ts
+++ src/config/site.ts
@@ -86,6 +86,15 @@ export const products: Product[] = [
                     },
                 ],
             },
+            other: {
+                environments: [
+                    {
+                        name: "data dashboard",
+                        url: "https://example.google.xyz/",
+                        description: "database viewing dashbaord",
+                    },
+                ],
+            }
         },
     },
     {
